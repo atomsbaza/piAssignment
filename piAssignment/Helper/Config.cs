816b8e1c9a8b20e22a96301249b74a35baadcdb0
PISIT KOOLPLUKPOL <pisit.koolplukpol@outlook.com>
2024-01-21 20:52:02 +0700

    fear: update code
    add Oauth 2.0

---
+++ piAssignment/Helper/Config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using IdentityServer4.Models;
+
+namespace piAssignment.Helper
+{
+	public static class Config
+	{
+        public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "https://localhost:7269/",
+                ClientSecrets = { new Secret("your_client_secret".Sha256()) }, // Not surw where to get it
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = { "api" }
+            }
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("api", "Your API")
+            };
+    }
+}
+
--- piAssignment/Program.cs
+++ piAssignment/Program.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IdentityServer4.Test;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using piAssignment.EntityModels;
+using piAssignment.Helper;
 using piAssignment.Interface;
 using piAssignment.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<piDbContext>()
+        .AddDefaultTokenProviders();
+
+// IdentityServer4 configuration
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<ApplicationUser>() // Replace with your actual identity class
+    .AddInMemoryClients(Config.Clients)  // Configure clients, see step 3
+    .AddInMemoryIdentityResources(Config.IdentityResources)  // Configure identity resources, see step 3
+    .AddInMemoryApiResources(Config.ApiResources) // Configure API resources, see step 3
+    .AddDeveloperSigningCredential(); // For development purposes only, use a real certificate in production
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,14 +31,6 @@ builder.Services.AddSwaggerGen();
 // Add DbContext configuration
 var connectionString = builder.Configuration.GetConnectionString("piAssignment");
 builder.Services.AddDbContext<piDbContext>(options => options.UseSqlServer(connectionString));
-//if (builder.Environment.IsDevelopment())
-//{
-//    builder.Services.AddDbContext<piDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
-//}
-//else
-//{
-//    builder.Services.AddDbContext<piDbContext>(options => options.UseSqlServer(connectionString));
-//}
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
